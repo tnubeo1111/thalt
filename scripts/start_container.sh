c40d706cc62c8715b8365acf0ed13ffed8960c33
tnubeo1111 <pelu0377583903@gmail.com>
2025-06-02 18:39:30 +0700

    edit file stop_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -10,7 +10,7 @@ echo "$IMAGE_NAME"
 echo "$IMAGE_TAG"
 
 # Pull the Docker image from Docker Hub
-docker pull $DOCKER_REGISTRY_USERNAME/$IMAGE_NAME:$IMAGE_TAG
+sudo docker pull $DOCKER_REGISTRY_USERNAME/$IMAGE_NAME:$IMAGE_TAG
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 $DOCKER_REGISTRY_USERNAME/$IMAGE_NAME:$IMAGE_TAG
+sudo docker run -d -p 5000:5000 $DOCKER_REGISTRY_USERNAME/$IMAGE_NAME:$IMAGE_TAG
